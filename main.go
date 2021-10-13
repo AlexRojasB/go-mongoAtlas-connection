20f50268a1c47de5f2f0c4a77423afeb0091508f
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2021-10-12 22:58:15 -0600

    Connection to MongoAtlas

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
+import (
+	"log"
+	"context"
+	"time"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+)
 func main() {
-	
+	clientOptions := options.Client().ApplyURI("mongodb+srv://seke:upsi1234@cluster0.zbbel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+    	log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.D{})
+	if err != nil {
+    	log.Fatal(err)
+	}
+	fmt.Println(databases)
 }
+
+
+
