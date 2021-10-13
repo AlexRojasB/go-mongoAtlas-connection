eae0a5c3e48707799563388a33ea9d0752787322
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2021-10-12 23:20:03 -0600

    add repository and service

---
+++ repositories/user.repository/user.repository.go
@@ -0,0 +1,25 @@
+package user_repository
+import (
+	m "../../models"
+)
+
+func Create(user m.User) error {
+
+	return nil
+}
+
+func Read() (m.Users, error) {
+	var users m.Users
+
+	return users, nil
+}
+
+func Update(user m.User, userId string) error {
+
+	return nil
+}
+
+func Delete(userId string) error {
+
+	return nil
+}
